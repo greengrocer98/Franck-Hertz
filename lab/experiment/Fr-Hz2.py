51c008c2a825622086b7aa66e2e2b0339c623510
Maksim <esyunin.maks@mail.ru>
2018-11-06 15:38:42 +0300

    Revert "well done"
    This reverts commit a76deec3d1d6e29075bf5fd09c8bcd2d489c41b5.

--- lab/experiment/Fr-Hz2.py
+++ lab/experiment/Fr-Hz2.py
@@ -23,19 +23,16 @@ plt.rcParams['text.latex.preamble'] = [
 
 data = np.genfromtxt('FrHz2exp.csv', delimiter=';')
 exp = np.loadtxt('Fr-Hz2.csv', delimiter=';',skiprows=1,usecols=(0,1,3,4,6,7))
-data2 = np.genfromtxt('FrHz2expapprox.csv', delimiter=';')
+
 fig = plt.figure(figsize=(10,7))
 ax = fig.add_subplot(111)
 ax.errorbar(exp[:,0]/2,exp[:,1]/10,xerr=.5,yerr=.1,fmt='.',color='#FF7800',label=r'$\varphi_z =40 V$')
 ax.plot(data[0,:]/4,data[1,:]/1000,color='#000000')
-ax.plot(data2[0,:]/2,data2[1,:]/10,color='#FF00FF')
 ax.errorbar(exp[:,2]/2,exp[:,3]/10,xerr=.5,yerr=.1,fmt='.',color='#133CAC',label=r'$\varphi_z =45 V$')
 ax.errorbar(exp[:,4]/2,exp[:,5]/10,xerr=.5,yerr=.1,fmt='.',color='#00C618',label=r'$\varphi_z =50 V$')
 
 ax.set_ylabel(r'$i_a, mA$')
 ax.set_xlabel(r'$\varphi_y, V$')
-ax.set_ylim(bottom=0)
-ax.set_xlim(left=0)
 ax.grid()
 ax.legend()
 plt.savefig('Fr-Hz2.pdf')
